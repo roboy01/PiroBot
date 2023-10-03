cc9e5522b3a08039f457a66e886dcd51ba006b51
PIROXTG <143400450+PIROXTG@users.noreply.github.com>
2023-10-03 20:50:34 +0530

    fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -784,7 +784,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     InlineKeyboardButton('➕ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
                 ],[
                     InlineKeyboardButton('ℹ️ 𝖧𝖾𝗅𝗉', callback_data='help'),
-                    InlineKeyboardButton('😊 𝖠𝖻𝗈𝗎𝗍', callback_data='about'),
+                    InlineKeyboardButton('😊 𝖠𝖻𝗈𝗎𝗍', callback_data='about')
                 ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
